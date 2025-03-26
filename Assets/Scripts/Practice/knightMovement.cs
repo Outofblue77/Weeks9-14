4d1afbc20a8dff007f988155e60bd6395c0bcc97
Richard Murillo <murillri@shernet.sheridancollege.ca>
2025-03-26 10:56:07 -0400

    Added sound to when footsteps hit the ground. Also began on working on jump triggers. Added the trigger to start jump animation and fall animation. Working on visually moveing the character now

--- Assets/Scripts/Practice/knightMovement.cs
+++ Assets/Scripts/Practice/knightMovement.cs
@@ -6,8 +6,10 @@ public class knightMovement : MonoBehaviour
 {
     Animator animator;
     SpriteRenderer sr;
+    public AudioSource runningFootSteps;
     public float speed = 2;
     public bool canRun = true;
+    public bool falling = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,9 @@ public class knightMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         float direction = Input.GetAxis("Horizontal");
+        float height = Input.GetAxis("Vertical");
 
         sr.flipX = (direction < 0);
 
@@ -30,10 +34,22 @@ public class knightMovement : MonoBehaviour
             animator.SetTrigger("attack");
             canRun = false;
         }
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetTrigger("jump");
+            canRun = true;
+            transform.position += transform.up * height * speed * Time.deltaTime;
+        }
+        if (falling == true)
+        {
+            animator.SetTrigger("fall");
+            canRun = true;
+        }
 
         if (canRun == true)
         {
             transform.position += transform.right * direction * speed * Time.deltaTime;
+            falling = false;
         }
     }
 
@@ -41,5 +57,18 @@ public class knightMovement : MonoBehaviour
     {
         canRun = true;
     }
+
+    public void FootStep()
+    {
+        runningFootSteps.Play();
+        canRun = true;
+    }
+
+    public void JumpHasFinished()
+    {
+        falling = true;
+    }
+
+
 
 }
