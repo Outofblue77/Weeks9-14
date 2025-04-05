7dc112095de5ff59d035319f3f1ea304b85bb3e9
OUTOFTHEBLUE\whate <whateverfloatsyourboat077@gmail.com>
2025-04-04 23:22:25 -0400

    Created script the moves the character/customer to the left when they are clicked on. uses coroutine.

--- Assets/Assignment 3/Scripts/CustomerMovement.cs
+++ Assets/Assignment 3/Scripts/CustomerMovement.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomerMovement : MonoBehaviour
 {
-    public float speed = 1;
-    public void walkingAway ()
-    {
-        Vector3 pos = transform.position;
-        pos.x = speed * Time.deltaTime;
+    public Image uiCharacter; //Creating a placeholder to add the ui object to
+    public float speed = 0;   //Creating a placeholder to control the speed
+    public float distance;    //Track placement and movent through unity ui
 
-        transform.position = pos;
+    private RectTransform rectTransform; //Stores transform f the ui component
+
+    public void clicked()   //New void to call on when the character is clicked on
+    {
+        rectTransform = GetComponent<RectTransform>(); //get the UI Image 
+        StartCoroutine(moveLeft());  //Call to start the coroutine
+    }
+    
+    public IEnumerator moveLeft()  //Call to use coroutine
+    {
+        distance = 0f; //Set loop to start at 0
+        while (distance < 550) //Loop to move character. 550 indicating distance
+        {
+            float step = speed * Time.deltaTime;  //Add delta time to make movemnt consistent
+            rectTransform.anchoredPosition -= new Vector2(step, 0f);  //Update the UI placement 
+            distance += step;  //Add the distance moved to the ui image
+            yield return null; //Stop until next fram to continue while loop
+        }
+
     }
 }
